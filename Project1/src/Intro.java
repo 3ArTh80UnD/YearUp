22e3bf6d20c421d5db2a7173ffbea6c0ea526dee
Reginald <rsmith@atl.yearup.org>
2015-09-22 11:25:37 -0400

    My first java program.

---
+++ Project1/src/Intro.java
@@ -0,0 +1,11 @@
+
+public class Intro {
+
+	public static void main(String[] args) {
+		System.out.println("My name is Reginald");
+		System.out.println("My favorite movie is Pokemon 2000.");
+		System.out.println("A fact about me is that I currently have four pets.");
+
+	}
+
+}
