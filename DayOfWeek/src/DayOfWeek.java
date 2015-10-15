aa78008754f4622ab5d2174abb73b6942a056955
Reginald <rsmith@atl.yearup.org>
2015-10-15 09:22:57 -0400

    More confusing than it seemed in the flow chart.

---
+++ DayOfWeek/src/DayOfWeek.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class DayOfWeek 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a new day of the week:");
+		String dayOfWeek = sc.nextLine();
+		EvaluateDayOfWeek(dayOfWeek);
+	}
+
+	public static void EvaluateDayOfWeek(String dayOfWeek)
+	{
+		if (dayOfWeek != null)
+		{
+			if (!dayOfWeek.isEmpty())
+			{
+				if(dayOfWeek.equalsIgnoreCase("Monday") ||
+						dayOfWeek.equalsIgnoreCase("Tuesday") ||
+						dayOfWeek.equalsIgnoreCase("Wednesday") ||
+						dayOfWeek.equalsIgnoreCase("Thursday") ||
+						dayOfWeek.equalsIgnoreCase("Friday"))
+				{
+					System.out.println(dayOfWeek + " is a weekday.");
+				}
+				else if(dayOfWeek.equalsIgnoreCase("Saturday") ||
+						dayOfWeek.equalsIgnoreCase("Sunday"))
+				{
+					System.out.println(dayOfWeek + " is a weekend day.");
+				}
+				else
+				{
+					System.out.println("This is not a valid day of the week!");
+				}
+			}
+
+			else
+			{
+				System.out.println("You have not entered anything!!");
+			}
+		}
+		else
+		{
+			System.out.println("You have given a null string!!!");
+		}
+
+	}
+}
