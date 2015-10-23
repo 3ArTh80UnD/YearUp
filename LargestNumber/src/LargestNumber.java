fba9cabf619bb47aad0925a281606cc7b98f30cd
Reginald <rsmith@atl.yearup.org>
2015-10-23 10:03:03 -0400

    here we go

---
+++ LargestNumber/src/LargestNumber.java
@@ -0,0 +1,56 @@
+
+public class LargestNumber 
+{
+
+	public static void main(String[] args) 
+	{
+		int[] numbersArray = new int[] {-1, 34, 57, 89, 69, 72, -83};
+		FindLargestNumberUsingWhileLoop(numbersArray);
+		FindLargestNumberUsingForLoop(numbersArray);
+	}
+	
+	public static void FindLargestNumberUsingWhileLoop(int[] numbers)
+	{
+		if (numbers != null)
+		{
+			if (numbers.length > 0)
+			{
+				int i = 1;
+				int largestNumber = numbers[0];
+				
+				while (i < numbers.length);
+				{
+					if (largestNumber < numbers[i])
+					{
+						largestNumber = numbers[i];
+					}
+					i++;
+				}
+				System.out.println("Largest number using while loop is " + largestNumber);
+			}
+		}
+	}
+	public static void FindLargestNumberUsingForLoop(int[] numbers)
+	{
+		if (numbers != null)
+		{
+			if (numbers.length > 0)
+			{
+				int i = 1;
+				int largestNumber = numbers[0];
+				
+				for (i = 1; i < numbers.length; i++);
+				{
+					if (largestNumber < numbers[i])
+					{
+						largestNumber = numbers[i];
+					}
+					System.out.println("Largest number using while loop is " + largestNumber);
+
+				}
+			}
+			
+		}
+	}
+
+}
