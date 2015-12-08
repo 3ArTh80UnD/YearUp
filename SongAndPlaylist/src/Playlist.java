3cab4d5245afa2b2ef7828c0ccc888943ad10a5b
3ArTh80UnD <rsmith@atl.yearup.org>
2015-12-08 11:18:20 -0500

    Small aesthetic tweaks.

--- SongAndPlaylist/src/Playlist.java
+++ SongAndPlaylist/src/Playlist.java
@@ -12,15 +12,15 @@ public class Playlist
 	public static void InitializePlaylist()
 	//Populates the spaces of the m_song array.
 	{
-		m_song[0] = new Song(3, "hello", "Rj");
-		m_song[1] = new Song(8, "world", "Rj");
-		m_song[2] = new Song(1, "my", "Rj");
-		m_song[3] = new Song(2, "name", "Rj");
-		m_song[4] = new Song(7, "is", "Rj");
-		m_song[5] = new Song(10, "reginald", "Rj");
-		m_song[6] = new Song(4, "wendell", "Rj");
-		m_song[7] = new Song(6, "smith", "Rj");
-		m_song[8] = new Song(9, "junior", "Rj");
+		m_song[0] = new Song(3, "Hello", "Rj");
+		m_song[1] = new Song(8, "World", "Rj");
+		m_song[2] = new Song(1, "My", "Rj");
+		m_song[3] = new Song(2, "Name", "Rj");
+		m_song[4] = new Song(7, "Is", "Rj");
+		m_song[5] = new Song(10, "Reginald", "Rj");
+		m_song[6] = new Song(4, "Wendell", "Rj");
+		m_song[7] = new Song(6, "Smith", "Rj");
+		m_song[8] = new Song(9, "Junior", "Rj");
 		m_song[9] = new Song(5, "Ok", "Rj");
 	}
 	
@@ -34,7 +34,7 @@ public class Playlist
 				String name = m_song[i].GetName();
 				String artist = m_song[i].GetArtist();
 				int rank = m_song[i].GetRanking();
-				System.out.println("The song name is: " + name + ". " + "The song's rank is: " + rank + ". " + "The song's artist is: " + artist + ".");
+				System.out.println("This is song number " + i + ". " + "The song name is " + name + ". " + "The song's rank is " + rank + ". " + "The song's artist is " + artist + ".");
 			}
 		}
 	}
